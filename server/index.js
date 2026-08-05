cc2b262fbb907fdcccdc3574e83a7f785e3ed7a1
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-08-05 11:48:15 +0500

    Starting server and Connect MongoDb

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 const express = require('express');
+
 const cors = require('cors');
+
+const dotenv = require('dotenv');
+
+const mongoose = require('mongoose');
+
+dotenv.config();
+
 const app = express();
-const port = 5000;
+
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected successfully')).catch((err) => console.log(err));
+
+const Port = process.env.PORT || 5000;
+
+app.listen(Port, () => {
+
+  console.log(`Server is running on port ${Port}`);
+
+});  
