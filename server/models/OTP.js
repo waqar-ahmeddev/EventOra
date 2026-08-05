43383088413f333614146b8342f7433efd73ee2a
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-08-05 16:01:51 +0500

    Build Api > /register , /login,/verify-otp and now testing these api in postman to solves the error that can b occur in this project

---
+++ server/models/OTP.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const otpSchema = new mongoose.Schema({
+    email: {
+        type: String,   
+        required: true
+    },
+    otp: {  
+        type: String,
+        required: true
+    },
+    action : {
+        type: String,
+        enum : ['account_verification', 'event_booking'],
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 300 // OTP expires after 5 minutes
+    }
+});
