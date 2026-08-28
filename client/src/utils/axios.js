75b5a175a5837d20d080858c371b880de9859bd9
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-08-28 22:55:40 +0500

    Update backend API URL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,15 +1,21 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://eventora-production-31e4.up.railway.app/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use(
+  (config) => {
     const token = localStorage.getItem('token');
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-});
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
