b106cde7ec36761752f752ae31f45ea57946d1a0
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-09-01 00:27:38 +0500

    add /api prefix to axios baseURL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: import.meta.env.VITE_API_BASE_URL || 'https://eventora-production-7f78.up.railway.app'
+   baseURL: import.meta.env.VITE_API_BASE_URL || 'https://eventora-production-7f78.up.railway.app/api'
 });
 
 api.interceptors.request.use((config) => {
