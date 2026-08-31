57760ef8df45fdd7e5d2922438756af71137654a
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-09-01 00:15:56 +0500

    fix axios url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+   baseURL: import.meta.env.VITE_API_BASE_URL || 'https://eventora-production-7f78.up.railway.app'
 });
 
 api.interceptors.request.use((config) => {
